b3786115ceb0ce6290da08e7e0f16b73b88b7734
Caitlin Woodward <caitlin.woodward@gmail.com>
2015-08-24 11:51:43 -0700

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BallparkClient::Application.config.secret_token = 'cd1af745389fe167844be62534c67700a4c9b372f75bbc01a8ba7d6273519786dbcc5991a8918af47aafea2c77a2edaf494e010fcb1fdbf441226f37af821edb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+BallparkClient::Application.config.session_store :cookie_store, key: '_ballpark-client_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# BallparkClient::Application.config.session_store :active_record_store
